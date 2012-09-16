14921f5acbdd95e35944fd6160afdc4b09ff008c
David Oberhollenzer <david.oberhollenzer@tele2.at>
2012-09-16 20:09:31 +0200

    Moved widget implementations into seperate subdirectory

--- compile.sh
+++ compile.sh
@@ -17,10 +17,14 @@ if [ "$?" == "127" ]; then
 fi
 
 # Common source code files
-SOURCE_COMMON="test.c src/widget.c src/progress_bar.c src/font.c src/rect.c
-                      src/widget_manager.c src/static_text.c src/button.c
-                      src/skin.c src/link.c src/image.c src/edit_box.c
-                      src/frame.c src/scroll_bar.c src/group_box.c src/tab.c"
+SOURCE_WIDGETS="src/widgets/progress_bar.c src/widgets/static_text.c
+                src/widgets/button.c src/widgets/image.c
+                src/widgets/edit_box.c src/widgets/frame.c
+                src/widgets/scroll_bar.c src/widgets/group_box.c
+                src/widgets/tab.c"
+
+SOURCE_COMMON="test.c src/widget.c src/font.c src/rect.c src/widget_manager.c
+                      src/skin.c src/link.c $SOURCE_WIDGETS"
 
 # Platform specific source code files
 SOURCE_X11="src/X11/window.c src/X11/keycode_translate.c src/X11/canvas.c"
@@ -39,12 +43,12 @@ if [ ! -f ./compile.sh ]; then
     echo "Error: script must be run from it's directory!"
 else
     gcc $SOURCE_COMMON $SOURCE_X11 $CFLAGS $INCLUDE_X11 $LIBS_X11\
-        -o build/a.out
+        -Isrc/ -o build/a.out
 
     $MINGW $SOURCE_COMMON $SOURCE_WIN $CFLAGS $INCLUDE_WIN $LIBS_WIN32\
-           -o build/a.exe
+           -Isrc/ -o build/a.exe
 
     $MINGW64 $SOURCE_COMMON $SOURCE_WIN $CFLAGS $INCLUDE_WIN $LIBS_WIN64\
-             -o build/a64.exe
+             -Isrc/ -o build/a64.exe
 fi
 
