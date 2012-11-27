588755513e683c93479dab9076d0b34a116bb721
David Oberhollenzer <david.oberhollenzer@tele2.at>
2012-11-27 16:58:02 +0100

    Splitted WinDOS and UNIX build systems and added pretty printing
     - build systems are now seperate scripts per target
     - build systems pretty print what they are doing and exit on error

--- compile_mingw.sh
+++ compile_mingw.sh
@@ -2,9 +2,9 @@
 
 # Flags for the C compiler
 CFLAGS="-ansi -pedantic -Werror -Wall -Wextra -Wshadow -Wwrite-strings
-        -I./include -ggdb"
+        -I./include -Ibuild/win_dep/include -mwindows -mwindows -ggdb"
 
-# Compile a list of files. $1: additional compiler flags, $2: list of files,
+# Compile a list of files. $1: prefix, $2: list of files,
 #                          $3: object directory, $4: object prefix
 #
 # Only compiles files if the coresponding object file does not exist or is
@@ -25,7 +25,7 @@ compile_files( )
         # compile if object file is older than source code file
         if [ $objtime -lt $srctime ]; then
             echo -e "\e[32m$f\e[0m"
-            gcc $CFLAGS $1 -c $f -o $obj
+            "$1"gcc $CFLAGS -c $f -o $obj
 
             # On error, exit
             if [ $? -ne 0 ]; then
@@ -35,7 +35,8 @@ compile_files( )
     done
 }
 
-# Create a static library from object files. $1: object dir, $2: library name
+# Create a static library from object files. $1: object dir, $2: library name,
+#                                            $3: prefix
 #
 # Simply glues all object files (*.o) within a source directory together to a
 # single static library.
@@ -44,14 +45,27 @@ create_library( )
     echo -e "\e[31m***** creating static library $2 *****\e[0m"
     cur=$(pwd)              # store working directory
     cd build/obj/$1         # go to object file directory
-    ar rcs $2 $(ls *.o)     # "ar" all *.o files to an archive
-    ranlib $2               # run "ranlib" on the resulting archive
+    "$3"ar rcs $2 $(ls *.o) # "ar" all *.o files to an archive
+    "$3"ranlib $2           # run "ranlib" on the resulting archive
     cd $cur                 # go back to stored working directory
 }
 
+################################# find mingw #################################
+PREFIX="i586-mingw32msvc-"
+PREFIX64="amd64-mingw32msvc-"
+
+"$PREFIX"gcc >/dev/null 2>&1
+if [ "$?" == "127" ]; then
+    PREFIX="i486-mingw32-"
+fi
+
+"$PREFIX64"gcc >/dev/null 2>&1
+if [ "$?" == "127" ]; then
+    PREFIX64="x86_64-w64-mingw32-"
+fi
+
 ############################# source code files #############################
 
-# Common source code files
 SOURCE_WIDGETS="src/widgets/progress_bar.c src/widgets/static_text.c
                 src/widgets/button.c src/widgets/image.c
                 src/widgets/edit_box.c src/widgets/frame.c
@@ -64,19 +78,20 @@ SOURCE_COMMON="src/widget.c src/font.c src/rect.c src/widget_manager.c
                src/skin.c src/filesystem.c src/canvas.c src/window.c
                src/screen.c $SOURCE_WIDGETS $SOURCE_OPENGL"
 
-# Platform specific stuff
-SOURCE_X11="src/X11/window.c src/X11/keycode_translate.c src/X11/canvas.c
-            src/X11/platform.c src/X11/opengl.c"
+SOURCE_PLATFORM="src/WIN32/window.c src/WIN32/canvas.c src/WIN32/platform.c
+                 src/WIN32/opengl.c"
 
-INCLUDE_X11="-I/usr/include -I/usr/include/freetype2"
-LIBS_X11="-lX11 -lfreetype -lGL"
+####################### Platform specific dependencies #######################
+LIBS32="-Lbuild/win_dep/x86 -llibfreetype -lgdi32 -lopengl32"
+LIBS64="-Lbuild/win_dep/x64 -llibfreetype -lgdi32 -lopengl32"
 
-# Test application sources
+########################## Test application sources ##########################
 SOURCE_TEST="test/test.c test/test_gl.c"
 
 ############################# Do the compilation #############################
 
-# compile test apps. $1: additional flags, $2: obj dir, $3: app postfix
+# compile test apps. $1: compiler prefix, $2: obj dir, $3: dependencies,
+#                    $4: app postfix
 compile_tests( )
 {
     echo -e "\e[31m***** compiling test and demo programs *****\e[0m"
@@ -89,8 +104,8 @@ compile_tests( )
     do
         name=$(basename $f ".o")
 
-        echo -e "\e[31m***** linking $name$3 *****\e[0m"
-        gcc $1 $f -Lbuild/obj/$2 -lsgui -o build/$name$3
+        echo -e "\e[31m***** linking $name$4 *****\e[0m"
+        "$1"gcc $f -Lbuild/obj/$2 -lsgui $3 -o build/$name$4
 
         # On error, exit
         if [ $? -ne 0 ]; then
@@ -100,30 +115,30 @@ compile_tests( )
 }
 
 # Do it
-if [ ! -f ./compile.sh ]; then
+if [ ! -f ./compile_mingw.sh ]; then
     echo -e "\e[01;31mERROR\e[0m script must be run from it's directory!"
 else
-    mkdir -p "build/obj/unix32/test"
-    mkdir -p "build/obj/unix64/test"
+    mkdir -p "build/obj/win32/test"
+    mkdir -p "build/obj/win64/test"
 
-    ######### unix32/X11 #########
-    echo -e "\e[0m * Compiling for x86, UNIX like system"
+    ########### win 32 ###########
+    echo -e "\e[0m * Compiling for x86, Windows operating system"
 
-    compile_files "-m32 $INCLUDE_X11" "$SOURCE_COMMON" "unix32"
-    compile_files "-m32 $INCLUDE_X11" "$SOURCE_X11"    "unix32" "x11_"
+    compile_files "$PREFIX" "$SOURCE_COMMON"   "win32"
+    compile_files "$PREFIX" "$SOURCE_PLATFORM" "win32" "w32_"
 
-    create_library "unix32" "libsgui.a"
+    create_library "win32" "libsgui.a" "$PREFIX"
 
-    compile_tests "-m32 $LIBS_X11" "unix32" "_unix32"
+    compile_tests "$PREFIX" "win32" "$LIBS32" "_w32.exe"
 
-    ######### unix64/X11 #########
-    echo -e "\e[0m * Compiling for x86_64, UNIX like system"
+    ########### win 64 ###########
+    echo -e "\e[0m * Compiling for x86_64, Windows operating system"
 
-    compile_files "-m64 $INCLUDE_X11" "$SOURCE_COMMON" "unix64"
-    compile_files "-m64 $INCLUDE_X11" "$SOURCE_X11"    "unix64" "x11_"
+    compile_files "$PREFIX64" "$SOURCE_COMMON"   "win64"
+    compile_files "$PREFIX64" "$SOURCE_PLATFORM" "win64" "w32_"
 
-    create_library "unix64" "libsgui.a"
+    create_library "win64" "libsgui.a" "$PREFIX64"
 
-    compile_tests "-m64 $LIBS_X11" "unix64" "_unix64"
+    compile_tests "$PREFIX64" "win64" "$LIBS64" "_w64.exe"
 fi
 
