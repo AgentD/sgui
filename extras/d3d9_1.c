f1c297aa06ddfc240a6f54fa0f785f3825c68a14
David Oberhollenzer <david.oberhollenzer@tele2.at>
2014-07-31 15:16:07 +0200

    Added (not really rotating) foreground triangle to d3d9_1 sample
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- extras/d3d9_1.c
+++ extras/d3d9_1.c
@@ -3,13 +3,33 @@
 
 
 
+typedef struct
+{
+    FLOAT x, y, z, rhw;
+    DWORD color;
+}
+CUSTOMVERTEX;
+
+CUSTOMVERTEX vertices[] =
+{
+    { 150.0f,  50.0f, 1.0f, 1.0f, D3DCOLOR_XRGB(0, 0, 255) },
+    { 250.0f, 250.0f, 1.0f, 1.0f, D3DCOLOR_XRGB(0, 255, 0) },
+    {  50.0f, 250.0f, 1.0f, 1.0f, D3DCOLOR_XRGB(255, 0, 0) }
+};
+
+#define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+
+
+
 int main( void )
 {
     int r=0, g=0, b=0, dr=1, dg=0, db=0;
+    LPDIRECT3DVERTEXBUFFER9 v_buffer;
     sgui_window_description desc;
     sgui_d3d9_context* ctx;
     IDirect3DDevice9* dev;
     sgui_window* wnd;
+    VOID* pVoid;
 
     sgui_init( );
 
@@ -33,10 +53,21 @@ int main( void )
     sgui_window_set_visible( wnd, SGUI_VISIBLE );
     sgui_window_set_vsync( wnd, 1 );
 
+    /* get the device context, set new present parameters */
     ctx = (sgui_d3d9_context*)sgui_window_get_context( wnd );
     dev = ctx->device;
 
     IDirect3DDevice9_Reset( dev, &ctx->present );
+
+    /* create a vertex buffer */
+    IDirect3DDevice9_CreateVertexBuffer( dev, 3*sizeof(CUSTOMVERTEX), 0,
+                                         CUSTOMFVF, D3DPOOL_MANAGED,
+                                         &v_buffer, NULL );
+
+    /* load vertex data */
+    IDirect3DVertexBuffer9_Lock( v_buffer, 0, 0, (void**)&pVoid, 0 );
+    memcpy( pVoid, vertices, sizeof(vertices) );
+    IDirect3DVertexBuffer9_Unlock( v_buffer );
 
     /* main loop */
     while( sgui_main_loop_step( ) )
@@ -55,19 +86,28 @@ int main( void )
         if( b==0 && db<0 ) { dr=1; dg= 0; db= 0; }
 
         /* draw scene */
-        IDirect3DDevice9_Clear( dev, 0, NULL, D3DCLEAR_TARGET,
+        IDirect3DDevice9_Clear( dev, 0, NULL,
+                                D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER,
                                 D3DCOLOR_XRGB(r, g, b), 1.0f, 0 );
 
-        /* XXX: there should be a rotating triangle here,
-                but my Direct3D-9-foo sucks */
+        IDirect3DDevice9_BeginScene( dev );
+
+        IDirect3DDevice9_SetFVF( dev, CUSTOMFVF );
+        IDirect3DDevice9_SetStreamSource( dev, 0, v_buffer, 0,
+                                          sizeof(CUSTOMVERTEX) );
+
+        IDirect3DDevice9_DrawPrimitive( dev, D3DPT_TRIANGLELIST, 0, 1 );
+
+        IDirect3DDevice9_EndScene( dev );
 
         sgui_window_swap_buffers( wnd );
     }
 
     /* clean up */
+    IDirect3DVertexBuffer9_Release( v_buffer );
+
     sgui_window_destroy( wnd );
     sgui_deinit( );
-
     return 0;
 }
 
